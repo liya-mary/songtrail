592ca3ab9b67f9c431299abf59dbe46c8125e305
eric-gebus <ericgebus@gmail.com>
2025-04-19 16:21:20 +0100

    Add album cover in NowPplaying

--- client/src/components/NowPlaying.jsx
+++ client/src/components/NowPlaying.jsx
@@ -6,10 +6,16 @@ export function NowPlaying({track}) {
 console.log(track)
   return (
     <>
-      <div className="now-playing">
-      <p className="track-title">{track.name || track.title}</p>
-      <p className="track-artist">{track.artists ? track.artists.map(a => a.name).join(', ') : track.artist}</p>
-    </div>
+      <div className="now-playing-container">
+        <img
+          src={track?.album?.images[0]?.url || "#"}
+          className="now-playing-image"
+        />
+        <div className="song-details">
+          <p className="track-title">{track.name || track.title}</p>
+          <p className="track-artist">{track.artists ? track.artists.map(a => a.name).join(', ') : track.artist}</p>
+        </div>
+      </div>
     </>
   )
 }
