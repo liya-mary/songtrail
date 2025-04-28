7358ff32174eb4e0180c95c517aaa03610f3fac1
tylaur <tylerjmunz@gmail.com>
2025-04-28 11:16:19 -0400

    initial testing setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,12 @@
-
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [basicSsl()],
+  plugins: [react(), basicSsl()],
+  test: {
+    environment: 'jsdom',
+  },
   server: {
     proxy: {
       '/auth': {
