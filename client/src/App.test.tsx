bd793992821e5933461a7a7e120bfd598074a9da
tylaur <tylerjmunz@gmail.com>
2025-04-29 04:28:20 -0400

    mocked geolocation

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,14 +1,50 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import App from './App';
+import { vi } from 'vitest';
+
+beforeAll(() => {
+
+  global.navigator = {
+    ...global.navigator,
+    geolocation: {
+      ...global.navigator.geolocation,
+      getCurrentPosition: vi.fn().mockImplementation(
+        (success: PositionCallback) => {
+          success({
+            coords: {
+              latitude: 51.505,
+              longitude: -0.09,
+              accuracy: 100,
+              altitude: null,
+              altitudeAccuracy: null,
+              heading: null,
+              speed: null,
+              toJSON: function () {
+                return {};
+              }
+            },
+            toJSON: function () {
+              return {};
+            },
+            timestamp: Date.now()
+          });
+        }
+      )
+    }
+  };
+
+});
 
 describe('App', () => {
-  it('renders headline', () => {
-    // render(<App/>);
+  it('renders the application properly without error', () => {
+    render(<App/>);
 
-    // screen.debug();
+    let loginButton = screen.getByText('Login with Spotify');
+    expect(loginButton).toBeTruthy();
 
-    // let element = screen.getByText("Login with Spotify");
-    // check if App components renders headline
+    fireEvent.mouseDown(loginButton);
+
+    screen.debug();
   });
 });
